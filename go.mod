eb1865c660f776e4c700e7b6c6bc1bfb8ef97676
James Cox <16992818+jcox250@users.noreply.github.com>
2021-03-23 12:10:15 +0000

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/jcox250/kinesis-pull
+
+go 1.15
+
+require (
+	github.com/aws/aws-sdk-go v1.37.20
+	github.com/flexera/micro v0.0.0-20210215150234-f6a21a440d72
+)
---
+++ main.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	stdlog "log"
+	"os"
+	"time"
+
+	"github.com/flexera/micro/log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kinesis"
+)
+
+var (
+	streamName        string
+	roleArn           string
+	shardIteratorType string
+	timestamp         string
+	awsRegion         string
+	debug             bool
+)
+
+func init() {
+	flag.StringVar(&streamName, "stream-name", "", "name of the kinesis stream")
+	flag.StringVar(&roleArn, "role-arn", "", "role to assume")
+	flag.StringVar(&shardIteratorType, "shard-iterator-type", "", "shard iterator type e.g TRIM_HORIZON")
+	flag.StringVar(&timestamp, "timestamp", "", "timestamp to pass if you use AT_TIMESTAMP for the shard-iterator-type")
+	flag.StringVar(&awsRegion, "aws-region", "", "aws region e.g us-east-1")
+	flag.BoolVar(&debug, "debug", false, "enables debug logs")
+	flag.Parse()
+}
+
+func main() {
+	ctx := setupLogger(context.Background(), debug)
+	kc, err := NewKinesisClient(KinesisConfig{
+		StreamName:        streamName,
+		RoleArn:           roleArn,
+		ShardIteratorType: shardIteratorType,
+		AWSRegion:         awsRegion,
+		Timestamp:         timestamp,
+	})
+	if err != nil {
+		stdlog.Fatal(err)
+	}
+
+	kc.Sub(ctx)
+}
+
+func setupLogger(ctx context.Context, debug bool) context.Context {
+	ctx = log.New(ctx, log.NewSyncWriter(os.Stderr))
+	if debug {
+		ctx = log.EnableDebug(ctx, debug)
+	}
+	return ctx
+}
+
+type KinesisConfig struct {
+	StreamName        string
+	RoleArn           string
+	ShardIteratorType string
+	AWSRegion         string
+	Shards            []string
+	Timestamp         string
+}
+
+type KinesisClient struct {
+	client            *kinesis.Kinesis
+	streamName        string
+	shards            []string
+	shardIteratorType string
+	timestamp         string
+}
+
+func NewKinesisClient(k KinesisConfig) (*KinesisClient, error) {
+	s := session.Must(session.NewSession())
+	creds := stscreds.NewCredentials(s, k.RoleArn)
+
+	kc := &KinesisClient{
+		client:            kinesis.New(s, &aws.Config{Region: aws.String(k.AWSRegion), Credentials: creds}),
+		streamName:        k.StreamName,
+		shards:            k.Shards,
+		shardIteratorType: k.ShardIteratorType,
+		timestamp:         k.Timestamp,
+	}
+
+	if len(kc.shards) == 0 {
+		shards, err := kc.getShards()
+		if err != nil {
+			return nil, err
+		}
+		kc.shards = shards
+	}
+
+	return kc, nil
+}
+
+func (k *KinesisClient) Sub(ctx context.Context) {
+	for _, shard := range k.shards {
+		if err := k.readShard(ctx, shard); err != nil {
+			log.Error(ctx, "msg", "failed to read shard", "shardID", shard, "err", err)
+		}
+	}
+}
+
+func (k *KinesisClient) readShard(ctx context.Context, shardID string) error {
+	iterator, err := k.getShardIterator(shardID)
+	if err != nil {
+		return err
+	}
+
+	for {
+		out, err := k.client.GetRecords(&kinesis.GetRecordsInput{
+			ShardIterator: aws.String(iterator),
+		})
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				// If throttled then wait and try again
+				case kinesis.ErrCodeProvisionedThroughputExceededException, kinesis.ErrCodeKMSThrottlingException:
+					time.Sleep(300 * time.Millisecond)
+					continue
+				}
+			}
+			log.Error(ctx, "msg", "failed to get records for shard")
+			return err
+		}
+
+		if len(out.Records) > 0 {
+			for _, record := range out.Records {
+				fmt.Println(string(record.Data))
+				if record.ApproximateArrivalTimestamp != nil {
+					fmt.Println("WrittenAt: ", *record.ApproximateArrivalTimestamp)
+				}
+				fmt.Println("")
+			}
+
+		}
+
+		if out.MillisBehindLatest != nil {
+			log.Debug(ctx, "MillisBehindLatest", *out.MillisBehindLatest)
+		}
+
+		if out.NextShardIterator == nil {
+			log.Error(ctx, "msg", "shard closed")
+			return nil
+		}
+		iterator = *out.NextShardIterator
+	}
+}
+
+func (k *KinesisClient) getShards() ([]string, error) {
+	res, err := k.client.ListShards(&kinesis.ListShardsInput{
+		StreamName: aws.String(k.streamName),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list shards: %s", err)
+	}
+
+	shards := []string{}
+	for _, s := range res.Shards {
+		shards = append(shards, *s.ShardId)
+	}
+	return shards, nil
+}
+
+func (k *KinesisClient) getShardIterator(shardID string) (string, error) {
+	var timestamp *time.Time
+	if k.timestamp != "" && k.shardIteratorType == kinesis.ShardIteratorTypeAtTimestamp {
+		t, err := time.Parse(time.RFC3339, k.timestamp)
+		if err != nil {
+			return "", fmt.Errorf("failed to parse timestamp: %s", err)
+		}
+		timestamp = &t
+	}
+	input := &kinesis.GetShardIteratorInput{
+		ShardId:           aws.String(shardID),
+		ShardIteratorType: aws.String(k.shardIteratorType),
+		StreamName:        aws.String(k.streamName),
+		Timestamp:         timestamp,
+	}
+
+	out, err := k.client.GetShardIterator(input)
+	if err != nil {
+		return "", err
+	}
+
+	return *out.ShardIterator, nil
+}
